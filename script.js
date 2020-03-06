284d07f6f4d8740dc84c8a8a5b49836fe67c3550
Tan Le-Dang <tledang@rsglab.com>
2020-03-06 17:47:15 -0500

    created dropdown menu that gets an image of that breed of dog

--- script.js
+++ script.js
@@ -11,4 +11,25 @@ function changeText(){
             document.getElementById("image").appendChild(dogPic);
             button.innerHTML = "Generate doggo"
         })
-}
+};
+
+axios.get('https://dog.ceo/api/breeds/list')
+    .then(function(response){
+        console.log(response)
+        response.data.message.map(function(breed){
+            var optionHTML = document.createElement("option");
+            optionHTML.setAttribute("value", `${breed}`);
+            optionHTML.innerHTML = `${breed}`;
+            document.getElementById("selectlist").appendChild(optionHTML);
+        })
+    });
+
+document.getElementById("selectlist").addEventListener("change", function(){
+    axios.get(`https://dog.ceo/api/breed/${this.value}/images/random`)
+        .then(function(response){
+            document.getElementById("image").innerHTML = "";
+            console.log(response.data);
+            var dogPic = document.createElement('img');
+            dogPic.setAttribute("src", `${response.data.message}`);
+            document.getElementById("image").appendChild(dogPic);
+})})
